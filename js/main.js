29047025e8f2e1199ceabbeff4bd9f3b4d030bcd
Christina Lin <christinalin31@gmail.com>
2017-10-02 08:57:19 -0700

    third commit

--- js/main.js
+++ js/main.js
@@ -13,105 +13,70 @@
 
 // restart gameboard button
 
-// if x + [0, 1, 2];
-// && turn is > 4;
-// then check if x occupies 0-2, 3-5, 6-8
-
-/*----- Variables -----*/
-var turnCount = 1;
-var gameBoard = new Array(9);
-var xMark = [];
-var oMark = [];
-var winning = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-
-var boxes = document.getElementsByClassName('box');
-for (var i = 0; i < boxes.length; i++){
-    boxes[i].addEventListener('click', addChar);
-}
-
-/*----- Event Listeners -----*/
-document.querySelector('td').addEventListener('click', addChar);
-document.querySelector('button').addEventListener('click', resetBoard);
-
 
 /*----- Functions -----*/
 
-//click function
-function mouseClick() {
-    //when this id is clicked
-    addChar(xMark, oMark);
+function startGame() {
+    for (var i = 1; i <= 9; i++) {
+        restart(i);
+}
 }
 
-function addChar(event) {     //check who's turn
-    if (turnCount % 2 !== 0) { //check if it is an odd turn for X
-    xMark.push(event.target.id);
-    if gameBoard[number] === null { //check array if index is null
-        addChar(gameBoard[i]); // add char to gambeboard at this index
-        gameBoard[number] = 'x';
-        //change view to display added char
-        // check for winner
-        // if turncount is greater than 4
-        if(turnCount > 4) {
-            console.log(xMark);
-            event.target.innerHTML = "X";
-        } else {
-            if(turnCount += 1) {
-            return idWinner = 'o';
-            }
-        }
-        idWinner('x')
-        } else {
-            // do ^ for O
-        }
-        idWinner('o')
+function nextMove(box) {
+    box.innerText = document.turn;
+    addChar();
+}
+
+function addChar() {     //check who's turn
+    if (document.turn == "X") {
+        document.turn = "O";
+    } else {
+        document.turn = "X"
     }
+}
+
+function restart(number) {
+    document.getElementById("box" + number).innerText = "";
 }
 
 // check if someone won
-function idWinner(checkFor){
-    var point = checkFor[checkFor.length - 1],
-    rest = checkFor.slice(0, checkFor.length - 1),
-    idWinner;
-    if (gameBoard[0, 1, 2] === 'x') {
-        win();
-    } else {
-        if (gameboard) {
-        return false;
-        }
-    }
+// function idWinner(checkFor){
+//     var point = checkFor[checkFor.length - 1],
+//     rest = checkFor.slice(0, checkFor.length - 1),
+//     idWinner;
+//     if (gameBoard[0, 1, 2] === 'x') {
+//         win();
+//     } else {
+//         if (gameboard) {
+//         return false;
+//         }
+//     }
     // loop thru array and check for conditions
 
     // set char to check for
     // if add turn check for x if even check for o
 
-    }
-}
+//     }
+// }
 
-// check for winning conditions
-    function checkWinners(marksArray, name) {
-        for (i = 0; i < winning.length; i++) {
-            var numWins = 0;
-        for (var j = 0; j < winning[i].length; j++) {
-            if(marksArray.indexOf(winning[i][j] !== -1)) {
-                numWins++;
-            }
-            if(numWins === 3) {
-                alert("Game over: " + name + " wins!");
-                playNewGame();
-            }
-        }
-    }
-}
+// // check for winning conditions
+//     function checkWinners(marksArray, name) {
+//         for (i = 0; i < winning.length; i++) {
+//             var numWins = 0;
+//         for (var j = 0; j < winning[i].length; j++) {
+//             if(marksArray.indexOf(winning[i][j] !== -1)) {
+//                 numWins++;
+//             }
+//             if(numWins === 3) {
+//                 alert("Game over: " + name + " wins!");
+//                 playNewGame();
+//             }
+//         }
+//     }
+// }
 
-function win() {
+// function win() {
     // show message
     // prevent player from adding more char
     // reset gameboard
-}
-
-function resetBoard() {
-    for (var i = 0; i < gameBoard.length; i++) {
-        gameBoard[i].innerHTML = '';
-    }
-    playerTurn = 0;
-}
+// }
